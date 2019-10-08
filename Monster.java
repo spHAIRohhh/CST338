78ef60bc3a1349757ed23fe256cb2fc101313283
sphairohhh <jamorrow@csumb.edu>
2019-10-07 17:18:00 -0700

    first commit

---
+++ Monster.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+
+public abstract class Monster {
+    private Integer hp;
+    private Integer xp;
+    private Integer maxHP;
+    private HashMap<String, Integer> items;
+
+    public Integer getHp() {
+        return hp;
+    }
+
+    public void setHp(Integer hp) {
+        this.hp = hp;
+    }
+
+    public Integer getXp() {
+        return xp;
+    }
+
+    public void setXp(Integer xp) {
+        this.xp = xp;
+    }
+
+    public Integer getMaxHP() {
+        return maxHP;
+    }
+
+    public void setMaxHP(Integer maxHP) {
+        this.maxHP = maxHP;
+    }
+
+    public HashMap<String, Integer> getItems() {
+        return items;
+    }
+
+    public void setItems(HashMap<String, Integer> items) {
+        this.items = items;
+    }
+
+    public Monster(Integer maxHP, Integer xp, HashMap<String, Integer> items){
+        this.maxHP = maxHP;
+        hp = this.maxHP;
+        this.xp = xp;
+        this.items = items;
+    }
+
+    @Override
+    public String toString(){
+        return
+                "hp=" + hp + "/" + maxHP;
+    }
+
+}
